f85d2181447316e308e331cf5e93ebc1b7801640
Joshua Baskaran <joshbaskaran@gmail.com>
2023-12-05 14:18:29 +0100

    Added crypt4gh to monorepo settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "FEGA-Norway"
+include("lib:crypt4gh")
 include("lib:clearinghouse")
+findProject(":lib:crypt4gh")?.name = "crypt4gh"
 findProject(":lib:clearinghouse")?.name = "clearinghouse"
