2309bcc58a65c9e8e313da1f931853f5198d84f3
Parisa Tejari <parisa.tejari@uib.no>
2023-12-08 11:17:58 +0100

    add clearinghouse module

---
+++ lib/clearinghouse/src/main/java/no/uio/ifi/clearinghouse/model/VisaType.java
@@ -0,0 +1,14 @@
+package no.uio.ifi.clearinghouse.model;
+
+/**
+ * Enum containing possible values of "type" property of the visa.
+ */
+public enum VisaType {
+
+    AffiliationAndRole,
+    AcceptedTermsAndPolicies,
+    ResearcherStatus,
+    ControlledAccessGrants,
+    LinkedIdentities
+
+}
