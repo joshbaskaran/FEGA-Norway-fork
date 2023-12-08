1a3a6f5e4d0d5bc1b92bb815e31031494452a2d5
Parisa Tejari <parisa.tejari@uib.no>
2023-12-08 11:17:58 +0100

    add clearinghouse module

--- lib/clearinghouse/src/main/java/no/uio/ifi/clearinghouse/Clearinghouse.java
+++ lib/clearinghouse/src/main/java/no/uio/ifi/clearinghouse/Clearinghouse.java
@@ -155,11 +155,11 @@ public enum Clearinghouse {
             PublicKey pubKey = keyFactory.generatePublic(keySpec);
 
             Jws<Claims> jws = Jwts.parser().verifyWith(pubKey).build().parseSignedClaims(visaToken);
-            Claims claims = jws.getPayload();
+            Claims claims = jws.getBody();
             if (claims.containsKey(GA_4_GH_VISA_V_1)) {
                 String visaJson = new Gson().toJson(claims.get(GA_4_GH_VISA_V_1));
                 Visa visa = new Gson().fromJson(visaJson, Visa.class);
-                visa.setSub(jws.getPayload().getSubject());
+                visa.setSub(jws.getBody().getSubject());
                 return Optional.of(visa);
             }
         } catch (SignatureException e) {
@@ -237,7 +237,7 @@ public enum Clearinghouse {
             PublicKey pubKey = keyFactory.generatePublic(keySpec);
 
             Jws<Claims> jws = Jwts.parser().verifyWith(pubKey).build().parseSignedClaims(accessToken);
-            String userInfoEndpoint = jws.getPayload().getIssuer() + USERINFO;
+            String userInfoEndpoint = jws.getBody().getIssuer() + USERINFO;
             Request request = new Request.Builder()
                     .header(AUTHORIZATION, BEARER + accessToken)
                     .url(userInfoEndpoint)
