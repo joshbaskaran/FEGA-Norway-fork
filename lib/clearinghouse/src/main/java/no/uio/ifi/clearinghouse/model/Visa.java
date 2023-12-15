1546050f6143a045ef87a3b58f6cf4dbfdd2362d
Parisa Tejari <parisa.tejari@uib.no>
2023-12-15 08:29:58 +0100

    spotlessCheck

--- lib/clearinghouse/src/main/java/no/uio/ifi/clearinghouse/model/Visa.java
+++ lib/clearinghouse/src/main/java/no/uio/ifi/clearinghouse/model/Visa.java
@@ -1,37 +1,29 @@
 package no.uio.ifi.clearinghouse.model;
 
+import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 
-import java.util.List;
-import java.util.Map;
-
-/**
- * POJO representing Crypt4GH visa.
- */
+/** POJO representing Crypt4GH visa. */
 @Data
 @ToString
 @NoArgsConstructor
 public class Visa {
 
-    private String sub;                         // JWT subject
+  private String sub; // JWT subject
 
-    @NonNull
-    private String type;                        // passport visa type
+  @NonNull private String type; // passport visa type
 
-    @NonNull
-    private Long asserted;                      // seconds since epoch
+  @NonNull private Long asserted; // seconds since epoch
 
-    @NonNull
-    private String value;                       // value string
+  @NonNull private String value; // value string
 
-    @NonNull
-    private String source;                      // source URL
+  @NonNull private String source; // source URL
 
-    private List<List<Map<?, ?>>> conditions;   // conditions
+  private List<List<Map<?, ?>>> conditions; // conditions
 
-    private String by;                          // by identifier
-
+  private String by; // by identifier
 }
