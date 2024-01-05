306b805c291e93026d02a3a52a2327eb25881c63
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-05 10:58:18 +0100

    Init Tsd-Api-Mock springboot app.

---
+++ services/tsd-api-mock/src/test/java/no/elixir/tsdapimock/TsdApiMockApplicationTest.java
@@ -0,0 +1,10 @@
+package no.elixir.tsdapimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsdApiMockApplicationTest {
+  @Test
+  void contextLoads() {}
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 rootProject.name = "FEGA-Norway"
 include("lib:crypt4gh")
 include("lib:clearinghouse")
+include("services:tsd-api-mock")
 findProject(":lib:crypt4gh")?.name = "crypt4gh"
 findProject(":lib:clearinghouse")?.name = "clearinghouse"
+findProject(":services:tsd-api-mock")?.name = "tsd-api-mock"
