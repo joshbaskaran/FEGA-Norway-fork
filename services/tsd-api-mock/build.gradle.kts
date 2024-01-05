58e809462af7f147d7c3282cb04e535f3edc6ffa
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-05 11:03:34 +0100

    Add jsonwebtoken dependencies to Tsd-Api-Mock
    In the build.gradle.kts file of the Tsd-Api-Mock, the Json Web Token (jsonwebtoken) dependencies have been added. This will allow for handling of JSON web tokens within the app. The additions include jjwt-api, jjwt-impl, and jjwt-jackson dependencies.

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -7,4 +7,7 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     runtimeOnly("com.h2database:h2")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
