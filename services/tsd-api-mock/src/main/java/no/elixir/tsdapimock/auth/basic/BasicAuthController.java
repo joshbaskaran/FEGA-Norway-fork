695e998c71de9739e4bcfa260453d58347bcdc84
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-08 13:42:47 +0100

    Add confirmation logic to signup process
    Expanded the signup feature in the BasicAuthService class to include a confirmation token generation and validation. Also introduced JWT service for token creation, new exceptions, and updated client repository.

--- services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/auth/basic/BasicAuthController.java
+++ services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/auth/basic/BasicAuthController.java
@@ -1,8 +1,11 @@
 package no.elixir.tsdapimock.auth.basic;
 
 import jakarta.validation.Valid;
+import java.util.NoSuchElementException;
+import no.elixir.tsdapimock.auth.basic.dto.SignupConfirmRequestDto;
 import no.elixir.tsdapimock.auth.basic.dto.SignupRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +25,23 @@ public class BasicAuthController {
       value = "/signup",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<?> singup(
+  public ResponseEntity<?> signup(
       @PathVariable String project, @Valid @RequestBody SignupRequestDto request) {
     var response = basicAuthService.signup(project, request);
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping(
+      value = "/signupconfirm",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> signupConfirm(
+      @PathVariable String project, @Valid @RequestBody SignupConfirmRequestDto request) {
+    try {
+      var response = basicAuthService.signupConfirm(project, request);
+      return ResponseEntity.ok(response.confirmationToken());
+    } catch (NoSuchElementException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
   }
 }
