5f97f48161021b1421d98d187b39e63a137a08c7
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-08 15:27:40 +0100

    Implement confirmation method in BasicAuthController and associated tests
    The confirmation method has been added to BasicAuthController to handle specific requests. This required the creation of new DTOs for the request and response as well as an update to BasicAuthService. The previous AuthControllerTest was deleted and a new WebLayerTest was created to test the updated functionality. The build.gradle.kts file was also updated to include a new dependency on Apache Commons Lang.

---
+++ services/tsd-api-mock/src/test/java/no/elixir/tsdapimock/WebLayerTest.java
@@ -0,0 +1,121 @@
+package no.elixir.tsdapimock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.elixir.tsdapimock.auth.basic.Client;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebLayerTest {
+
+  private static Client client;
+  private final String basicAuthUrl = "/v1/p-test/auth/basic";
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @BeforeAll
+  public static void setUp() {
+    client = Client.builder().name("test").email("test@example.com").build();
+  }
+
+  @Nested
+  class BasicAuth {
+    @Test
+    public void testSignup() throws Exception {
+      var requestBody =
+          new ObjectMapper()
+              .createObjectNode()
+              .put("client_name", client.getName())
+              .put("EMAIL", client.getEmail())
+              .toString();
+
+      var requestHeaders = new HttpHeaders();
+      requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+      var requestEntity = new HttpEntity<>(requestBody, requestHeaders);
+
+      ResponseEntity<String> response =
+          restTemplate.postForEntity(basicAuthUrl + "/signup", requestEntity, String.class);
+
+      assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+      assertThat(response.getBody()).isNotNull();
+
+      var responseJson = new ObjectMapper().readTree(response.getBody());
+
+      var clientId = responseJson.get("client_id");
+      assertThat(clientId).isNotNull();
+      assertThat(clientId.textValue()).isNotBlank();
+
+      client.setId(clientId.textValue());
+    }
+
+    @Nested
+    class SignupConfirmation {
+      @Test
+      public void testSignupConfirmation() throws Exception {
+        var requestBody =
+            new ObjectMapper()
+                .createObjectNode()
+                .put("client_id", client.getId())
+                .put("EMAIL", client.getEmail())
+                .put("client_name", client.getName())
+                .toString();
+
+        var requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        var requestEntity = new HttpEntity<>(requestBody, requestHeaders);
+
+        ResponseEntity<String> response =
+            restTemplate.postForEntity(
+                basicAuthUrl + "/signupconfirm", requestEntity, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+
+        var responseJson = new ObjectMapper().readTree(response.getBody());
+        var confirmationToken = responseJson.get("confirmation_token").asText();
+        assertThat(confirmationToken).isNotBlank();
+
+        client.setConfirmationToken(confirmationToken);
+      }
+
+      @Nested
+      class Confirmation {
+        @Test
+        public void testConfirmation() throws Exception {
+          var requestBody =
+              new ObjectMapper()
+                  .createObjectNode()
+                  .put("client_id", client.getId())
+                  .put("confirmation_token", client.getConfirmationToken())
+                  .toString();
+
+          var requestHeaders = new HttpHeaders();
+          requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+          var requestEntity = new HttpEntity<>(requestBody, requestHeaders);
+
+          ResponseEntity<String> response =
+              restTemplate.postForEntity(basicAuthUrl + "/confirm", requestEntity, String.class);
+
+          assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+          assertThat(response.getBody()).isNotNull();
+
+          var responseJson = new ObjectMapper().readTree(response.getBody());
+          var password = responseJson.get("password").asText();
+          assertThat(password).isNotBlank();
+
+          client.setPassword(password);
+        }
+      }
+    }
+  }
+}
