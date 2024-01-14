44ac7aa0f7bbd0347b10c2f5fb1b6149fd2e93c5
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-15 00:52:27 +0100

    Add missing header exception handler in TSD API mock
    This commit introduces an exception handler for missing request headers in validation of the TSD API mock. It sends a detailed error response including the name of the missing header, which enhances the error handling capacity of the service.

--- services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/validation/ValidationExceptionHandler.java
+++ services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/validation/ValidationExceptionHandler.java
@@ -1,11 +1,13 @@
 package no.elixir.tsdapimock.validation;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,6 +24,17 @@ public class ValidationExceptionHandler {
         e.getBindingResult().getAllErrors().stream()
             .map(ObjectError::getDefaultMessage)
             .collect(Collectors.toList()));
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MissingRequestHeaderException.class)
+  public ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(
+      MissingRequestHeaderException e) {
+    ErrorResponse error = new ErrorResponse();
+    error.setTimestamp(LocalDateTime.now());
+    error.setMessage("Missing Request Header Error");
+    error.setDetails(
+        Collections.singletonList("Required header '" + e.getHeaderName() + "' is missing."));
     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 }
