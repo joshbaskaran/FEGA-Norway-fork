4bc961d1d584d4cd353684a4104eaabd814fe73a
Joshua Baskaran <joshbaskaran@gmail.com>
2024-01-16 10:36:38 +0100

    Implement file upload functionality in TSD API Mock
    Added a new file, FileUploadMessageDto, and updated FilesService and FilesController to support file upload operations. FilesService now includes methods for uploading files and checks for proper authorization and filename. FilesController has been updated to handle PUT requests for file upload and response accordingly based on the result of the operation.

--- services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/files/FilesController.java
+++ services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/files/FilesController.java
@@ -1,8 +1,12 @@
 package no.elixir.tsdapimock.files;
 
+import java.io.InputStream;
+import no.elixir.tsdapimock.exceptions.CredentialsMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/{project}/files/")
@@ -12,5 +16,24 @@ public class FilesController {
   @Autowired
   public FilesController(FilesService filesService) {
     this.filesService = filesService;
+  }
+
+  @PutMapping(
+      value = "/stream",
+      consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> upload(
+      @PathVariable String project,
+      @RequestHeader("Authorization") String authorizationHeader,
+      @RequestHeader("filename") String fileName,
+      InputStream fileStream) {
+    try {
+      var response = filesService.upload(project, authorizationHeader, fileName, fileStream);
+      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    } catch (CredentialsMismatchException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
   }
 }
--- services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/files/FilesService.java
+++ services/tsd-api-mock/src/main/java/no/elixir/tsdapimock/files/FilesService.java
@@ -1,6 +1,47 @@
 package no.elixir.tsdapimock.files;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import lombok.extern.slf4j.Slf4j;
+import no.elixir.tsdapimock.exceptions.CredentialsMismatchException;
+import no.elixir.tsdapimock.files.dto.FileUploadMessageDto;
+import no.elixir.tsdapimock.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-public class FilesService {}
+public class FilesService {
+  private final JwtService jwtService;
+
+  @Value("${tsd.file.import}")
+  public String durableFileImport;
+
+  @Autowired
+  public FilesService(JwtService jwtService) {
+    this.jwtService = jwtService;
+  }
+
+  public FileUploadMessageDto upload(
+      String project, String authorizationHeader, String fileName, InputStream fileStream) {
+    if (!authorizationHeader.startsWith("Bearer ")) {
+      throw new IllegalArgumentException("Header must contain a bearer auth token");
+    }
+    if (!jwtService.verify(authorizationHeader)) {
+      throw new CredentialsMismatchException("Stream processing failed");
+    }
+
+    var path = Paths.get(String.format(durableFileImport, project), fileName);
+    try {
+      Files.copy(fileStream, path, StandardCopyOption.REPLACE_EXISTING);
+      log.info(path.getParent().toString());
+    } catch (IOException e) {
+      log.error(e.getMessage());
+    }
+    return new FileUploadMessageDto("Data Streamed");
+  }
+}
