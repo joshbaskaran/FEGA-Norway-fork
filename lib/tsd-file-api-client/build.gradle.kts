d7237751e4f221d0a85e3a07afdb0a3003e5b558
Pavel Vazquez Faci <79310152+pavelvazquez@users.noreply.github.com>
2024-01-29 19:37:37 +0100

    Update lib/tsd-file-api-client/build.gradle.kts
    removed java17 compatibility
    
    Co-authored-by: Joshua Baskaran <66942863+joshbaskaran@users.noreply.github.com>

--- lib/tsd-file-api-client/build.gradle.kts
+++ lib/tsd-file-api-client/build.gradle.kts
@@ -7,10 +7,6 @@ plugins {
 group = "elixir.no"
 version = "2.0.0"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
 
 repositories {
     mavenCentral()
