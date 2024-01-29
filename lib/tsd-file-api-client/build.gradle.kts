7dcf22a95dc621e3dde50d7f6ab7bbecdbff014b
pavelvazquez <pavelva@uio.no>
2024-01-29 19:32:33 +0100

    Updated lombok and okhttp dependencies

--- lib/tsd-file-api-client/build.gradle.kts
+++ lib/tsd-file-api-client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.4"
     id("formatting-conventions")
 }
 
@@ -16,15 +17,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.24")
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    implementation("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     implementation("org.apache.commons:commons-lang3:3.9")
     implementation("commons-io:commons-io:2.7")
     implementation("com.auth0:java-jwt:3.10.3")
     implementation("com.google.code.gson:gson:2.8.9")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0-RC1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.slf4j:slf4j-jdk14:1.7.28")
 
     testImplementation("junit:junit:4.13.1")
