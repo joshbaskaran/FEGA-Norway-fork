e140d8a83810d3c6172f8cb608a0182b46988559
Yasin <wytm97@protonmail.com>
2024-04-26 10:43:06 +0200

    fix: gradle test execution

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -10,6 +10,7 @@ version = "1.0-SNAPSHOT"
 dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("org.apache.commons:commons-lang3:3.14.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
