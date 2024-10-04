5c4d6c210b14acf1d25315cf8dca1a4c3099dccc
pavelvazquez <pavelva@uio.no>
2024-10-04 12:02:09 +0200

    Resolve conflict in build.gradle.kts by using main branch version

--- lib/tsd-file-api-client/build.gradle.kts
+++ lib/tsd-file-api-client/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     id("io.freefair.lombok") version "8.10"
     id("formatting-conventions")
+    id("maven-publish")
 }
 
 group = "elixir.no"
@@ -24,10 +25,28 @@ dependencies {
     api("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.slf4j:slf4j-jdk14:2.0.16")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "fega-norway-tsd-file-api-client"
+            url = uri("https://maven.pkg.github.com/ELIXIR-NO/FEGA-Norway")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
