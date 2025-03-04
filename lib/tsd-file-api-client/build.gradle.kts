dfe15902e74ddf478e9da4946129807c82992515
pavelvazquez <pavelva@uio.no>
2025-03-04 11:54:43 +0100

    refactor: replace Auth0 JWT.decode with JJWT decoding

--- lib/tsd-file-api-client/build.gradle.kts
+++ lib/tsd-file-api-client/build.gradle.kts
@@ -18,8 +18,11 @@ dependencies {
 
     implementation("org.apache.commons:commons-lang3:3.17.0")
     implementation("commons-io:commons-io:2.18.0")
-    implementation("com.auth0:java-jwt:4.5.0")
     implementation("com.google.code.gson:gson:2.12.1")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
 
     api("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.slf4j:slf4j-jdk14:2.0.17")
