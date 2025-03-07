4226e4fbbcec685b668ef42becbb5e5fa33a8616
Yasin <wytm97@protonmail.com>
2025-03-07 14:47:06 +0100

    fix(proxy): remove duplicate prefix config

--- services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/config/RabbitMQProperties.java
+++ services/localega-tsd-proxy/src/main/java/no/elixir/fega/ltp/config/RabbitMQProperties.java
@@ -2,12 +2,10 @@ package no.elixir.fega.ltp.config;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties(prefix = "mq")
 @Slf4j
 public class RabbitMQProperties {
 
