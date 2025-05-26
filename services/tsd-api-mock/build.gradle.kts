bd8680015e27550e63e0c20ca8f9b31c189a4afa
Parisa <parisa.tejari@uib.no>
2025-05-26 12:35:02 +0200

    add  persistence-api to tsd-api-mock to resolve the conflict in version

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("org.apache.commons:commons-lang3:3.17.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
 }
