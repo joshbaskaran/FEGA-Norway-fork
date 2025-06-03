0c62a35efd5c390be3b1d8f62c8b8b49db965475
Parisa <parisa.tejari@uib.no>
2025-06-03 09:13:17 +0200

    add spring-boot-test to tsd-api-mock

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -22,4 +22,5 @@ dependencies {
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.test)
 }
