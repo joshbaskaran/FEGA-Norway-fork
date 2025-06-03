762c8ef969a05f45a9d5f0c2fbda7f4fd6bdf926
Parisa <parisa.tejari@uib.no>
2025-06-03 08:38:10 +0200

    Update build.gradle.kts

--- services/tsd-api-mock/build.gradle.kts
+++ services/tsd-api-mock/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 group = "no.elixir"
 
 dependencies {
-    runtimeOnly("com.h2database:h2")
     implementation("org.apache.commons:commons-lang3:3.17.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
