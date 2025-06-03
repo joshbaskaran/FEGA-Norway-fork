b4d14d079563288119acea062239128e6da7f0ff
Parisa <parisa.tejari@uib.no>
2025-06-03 09:49:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,6 @@ group = "no.elixir"
 
 repositories { mavenCentral() }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 tasks.test { useJUnitPlatform() }
 
 tasks.wrapper {
